d0ec31ed0ff9e9fd640626e1b9b98b5082fc9f7b
Derry Ukere <ukderry@gmail.com>
2021-06-29 12:35:56 +0100

    added rout error handlers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,20 +1,34 @@
-import express from 'express';
+import express, {ErrorRequestHandler} from 'express';
 import cors from 'cors';
-import { handleRouteError } from '../handlers';
+// import { handleRouteError, } from '../handlers';
+// import { Error } from 'mongoose';
 
 const router: express.Router = express.Router();
+// const services = express();
 
 router.use(cors());
 
-router.use('/', (req, res) => {
-  res.send({ status: 'HOME' });
-});
+
 
 
 router.use('/health', (req, res) => {
+  console.log('called health route');
   res.send({ status: 'OK' });
 });
 
+// eslint-disable-next-line no-unused-vars
+const handleRouteError : ErrorRequestHandler = (error, req, res) => {
+  res.status(error.status || 500);
+  console.error(error);
+  return res.json({
+    error: {
+      message: error.message,
+    },
+  });
+
+};
+
 router.use(handleRouteError);
+
 
 export default router;
